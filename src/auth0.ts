e813c4af7da7bc42ceb63d8d0d07f967e2ea4b03
Jonas Silva <STRANG3R973@outlook.com>
2022-03-01 01:47:18 -0300

    Errors fixed

--- src/auth0.ts
+++ src/auth0.ts
@@ -1,8 +1,8 @@
 import { ManagementClient } from 'auth0'
 
 export const auth0Management = new ManagementClient({
-  clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID2,
-  clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET2,
+  clientId: process.env.AUTH0_CLIENT_ID2,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET2,
   domain: 'dev-zodr-hta.us.auth0.com',
   scope: 'read:users update:users'
 })
