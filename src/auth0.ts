9a7ffc848d4e66a71d999f984a46c18540e2396d
Jonas Silva <STRANG3R973@outlook.com>
2022-03-01 01:42:11 -0300

    Revert "Scope removed"
    This reverts commit 4eeb74d0898de306d4e377895fc315c5d05d0906.

--- src/auth0.ts
+++ src/auth0.ts
@@ -3,6 +3,6 @@ import { ManagementClient } from 'auth0'
 export const auth0Management = new ManagementClient({
   clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID2,
   clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET2,
-  domain: 'dev-zodr-hta.us.auth0.com'
-  // scope: 'read:users update:users'
+  domain: 'dev-zodr-hta.us.auth0.com',
+  scope: 'read:users update:users'
 })
